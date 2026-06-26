4b5cf68f849fa1f3cedbc3edf0088f93f98dc3c8
moad abdel <waheb.moad@gmail.com>
2026-06-26 16:32:00 +0200

    prisma config ts

---
+++ prisma.config.ts
@@ -0,0 +1,16 @@
+// Prisma configuration file.
+// Assumes the following are installed:
+//   npm install --save-dev prisma dotenv
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+    seed: "node prisma/seed.js",
+  },
+  datasource: {
+    url: process.env["DATABASE_URL"] as string,
+  },
+});
